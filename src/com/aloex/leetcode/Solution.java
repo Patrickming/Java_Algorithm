a47d702c6ff657ac358f43c1a18d8378b0bf9005
Patrick <patrickmingx@gmail.com>
2022-11-23 17:52:07 +0800

    t1030 commit

--- src/com/aloex/leetcode/Solution.java
+++ src/com/aloex/leetcode/Solution.java
@@ -1,10 +1,28 @@
 package com.aloex.leetcode;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * @author: xrm
  * @date: 2022/11/23 17:39
  * @description:
  */
-public class Solution {
-
+class Solution {
+    public int[][] allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
+        int[][] ret = new int[rows * cols][];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                ret[i * cols + j] = new int[]{i, j};
+            }
+        }
+        Arrays.sort(ret, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a, int[] b) {
+                return (Math.abs(a[0] - rCenter) + Math.abs(a[1] - cCenter)) - (Math.abs(b[0] - rCenter) + Math.abs(b[1] - cCenter));
+            }
+        });
+        return ret;
+    }
 }
+
